ab83f10d76f96477828fd9de84e2295c893358a1
Kensey McDowell <klm2cz@mtmail.mtsu.edu>
2025-10-01 16:59:02 -0500

    Front-End background, title, and log in

---
+++ my-library-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Navigation Bar */}
+      <nav className="navbar">
+        <div className="top-left-buttons">
+          <button>Sign In</button>
+          <button>Log In</button>
+        </div>
+        <header className="App-header">
+          <h1 className="main-title">Multi-Branch Library Management System</h1>
+        </header>
+      </nav>
+      <div class="box"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-library-app/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="top-left-buttons">
+        <button>Sign In</button>
+        <button>Log In</button>
+      </div>
+      <header className="App-header">
+        <h1 className="main-title">Multi-Branch Library Management System</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
